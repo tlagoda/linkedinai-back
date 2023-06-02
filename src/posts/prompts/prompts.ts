fcb1ef4aee70bdd70962905a991463cbbdb090e7
tlagoda <thibautlagoda@hotmail.fr>
2023-06-02 22:56:06 +0200

    add linkedin post prompt

---
+++ src/posts/prompts/prompts.ts
@@ -0,0 +1,7 @@
+export const prompt1 = `
+Tu es un super-assistant copywriter et tu vas m'aider à rédiger des posts Linkedin impactant.
+Je souhaite raconter une histoire drôle à la première personne du singulier sur le sujet du football américain.
+Le post devra contenir des sauts à la ligne afin d'allonger le temps de lecture et rendre l'histoire immersive.
+N'hésite pas à utiliser des emojis.
+Utilise un ton sportif.
+`;
