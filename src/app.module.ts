93905905190711ab0a957ef006b303875b621ea5
tlagoda <thibautlagoda@hotmail.fr>
2023-06-02 23:42:40 +0200

    linkedin controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,11 +4,13 @@ import { AppService } from './app.service';
 import { PostsController } from './posts/posts.controller';
 import { PostsService } from './posts/posts.service';
 import { ConfigModule } from '@nestjs/config';
+import { LinkedinController } from './linkedin/linkedin.controller';
+import { LinkedinService } from './linkedin/linkedin.service';
 
 
 @Module({
   imports: [ConfigModule.forRoot()],
-  controllers: [AppController, PostsController],
-  providers: [AppService, PostsService],
+  controllers: [AppController, PostsController, LinkedinController],
+  providers: [AppService, PostsService, LinkedinService],
 })
 export class AppModule {}
