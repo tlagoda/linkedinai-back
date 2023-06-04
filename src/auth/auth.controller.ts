16d8a9c7fc72d53f23807bf021b74906c30eb97d
tlagoda <thibautlagoda@hotmail.fr>
2023-06-04 10:03:57 +0200

    swap code for access token

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,31 @@
 import { Controller, Get, Req, Res } from '@nestjs/common';
 import { Request, Response } from 'express';
+import axios from 'axios';
 
 @Controller('auth')
 export class AuthController {
   @Get('linkedin/callback')
-  linkedinRedirect(@Req() req: Request, @Res() res: Response) {
-    // TODO
+  async linkedinRedirect(@Req() req: Request, @Res() res: Response) {
+    console.log('LinkedIn redirection');
+    const { code, state } = req.query;
+
+    // Swap code for access token
+    const tokenResponse = await axios.post(
+      'https://www.linkedin.com/oauth/v2/accessToken',
+      null,
+      {
+        params: {
+          grant_type: 'authorization_code',
+          code,
+          client_id: 'LINKEDIN_CLIENT_ID',
+          client_secret: 'LINKEDIN_CLIENT_SECRET',
+          redirect_uri: 'http://localhost:3000/auth/linkedin/callback',
+        },
+      },
+    );
+
+    const { access_token, expires_in } = tokenResponse.data;
+
+    res.send('LinkedIn callback successful!');
   }
 }
