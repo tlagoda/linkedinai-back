49bc75b08bdf886a09e3f34a15bbb90f0b44d85d
tlagoda <thibautlagoda@hotmail.fr>
2023-06-04 23:39:33 +0200

    fix prompt

--- src/posts/prompts/prompts.ts
+++ src/posts/prompts/prompts.ts
@@ -1,7 +1,7 @@
 export const prompt1 = `
 Tu es un super-assistant copywriter et tu vas m'aider à rédiger des posts Linkedin impactant.
-Je souhaite raconter une histoire drôle à la première personne du singulier sur le sujet du football américain.
+Je souhaite raconter une histoire drôle à la première personne du singulier sur le sujet de l'IA.
 Le post devra contenir des sauts à la ligne afin d'allonger le temps de lecture et rendre l'histoire immersive.
 N'hésite pas à utiliser des emojis.
-Utilise un ton sportif.
+Utilise un ton moqueur.
 `;
