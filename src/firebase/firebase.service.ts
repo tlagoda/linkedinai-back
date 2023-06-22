3be521e7eaee43765f0e6098a74e589f2af6c434
tlagoda <thibautlagoda@hotmail.fr>
2023-06-22 23:11:58 +0200

    firebase config base64

--- src/firebase/firebase.service.ts
+++ src/firebase/firebase.service.ts
@@ -7,26 +7,15 @@ export class FirebaseService {
   private db: FirebaseFirestore.Firestore;
 
   constructor(private configService: ConfigService) {
-    const privateKey = this.configService
-      .get<string>('FIREBASE_PRIVATE_KEY')
-      .replace(/\\n/g, '\n');
+    const firebaseConfigBase64 = this.configService.get<string>(
+      'FIREBASE_CONFIG_ENCODED',
+    );
+    const firebaseConfigJson = Buffer.from(
+      firebaseConfigBase64,
+      'base64',
+    ).toString('utf-8');
+    const serviceAccount = JSON.parse(firebaseConfigJson);
 
-    const serviceAccount = {
-      type: this.configService.get<string>('FIREBASE_TYPE'),
-      project_id: this.configService.get<string>('FIREBASE_PROJECT_ID'),
-      private_key_id: this.configService.get<string>('FIREBASE_PRIVATE_KEY_ID'),
-      private_key: privateKey,
-      client_email: this.configService.get<string>('FIREBASE_CLIENT_EMAIL'),
-      client_id: this.configService.get<string>('FIREBASE_CLIENT_ID'),
-      auth_uri: this.configService.get<string>('FIREBASE_AUTH_URI'),
-      token_uri: this.configService.get<string>('FIREBASE_TOKEN_URI'),
-      auth_provider_x509_cert_url: this.configService.get<string>(
-        'FIREBASE_AUTH_PROVIDER_X509_CERT_URL',
-      ),
-      client_x509_cert_url: this.configService.get<string>(
-        'FIREBASE_CLIENT_X509_CERT_URL',
-      ),
-    };
     admin.initializeApp({
       credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
     });
