7b16cdb944bc5b4d14597f2059ee679afdaa200b
tlagoda <thibautlagoda@hotmail.fr>
2023-06-27 13:39:47 +0200

    update accesToken and expiresIn from LinkedIn in db

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -2,10 +2,14 @@ import { AuthService } from './auth.service';
 import { Controller, Get, Req, Res } from '@nestjs/common';
 import { Request, Response } from 'express';
 import { globalVariable } from '../global/global';
+import { FirebaseService } from 'src/firebase/firebase.service';
 
 @Controller('auth')
 export class AuthController {
-  constructor(private readonly authService: AuthService) {}
+  constructor(
+    private readonly authService: AuthService,
+    private readonly firebaseService: FirebaseService,
+  ) {}
 
   @Get('linkedin/callback')
   async linkedinRedirect(@Req() req: Request, @Res() res: Response) {
@@ -19,17 +23,26 @@ export class AuthController {
 
       const { access_token: accessToken, expires_in: expiresIn } =
         tokenResponse.data;
-      globalVariable.accessToken = accessToken;
-      globalVariable.expiresIn = expiresIn;
 
       const userInformations =
         await this.authService.getLinkedInUserInformations();
 
-      globalVariable.userId = userInformations.data.id;
       await this.authService.updateUserLinkedInAuthorization(
         state as string,
         true,
       );
+
+      const db = this.firebaseService.getFirestore();
+      const userId = userInformations.data.id;
+      const userRef = db.collection('users').doc(userId);
+      await userRef.set(
+        {
+          accessToken,
+          expiresIn,
+        },
+        { merge: true },
+      );
+
       res.status(200).redirect('http://tldl.fr/generate');
     } catch (error) {
       console.error('Error during LinkedIn callback:', error);
