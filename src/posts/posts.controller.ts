9aa5907247d674b0a4e86fdd255167afc2d01e8f
tlagoda <thibautlagoda@hotmail.fr>
2023-07-18 19:30:19 +0200

    debug

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -40,7 +40,7 @@ export class PostsController {
 
     try {
       const response = await this.postsService.shareOnLinkedIn(
-        content,
+        'test',
         token,
         files,
       );
