538dbb4f3413939096953c7a79df32d8621fff94
tlagoda <thibautlagoda@hotmail.fr>
2023-07-18 20:51:19 +0200

    edit cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://www.tldl.fr', 'https://tldl.fr', 'http://localhost:3000'],
+    origin: ['http://tldl.fr', 'https://tldl.fr', 'http://localhost:3000'],
     allowedHeaders: 'Content-Type,Authorization',
     methods: 'GET,POST',
   });
