8a18956ff93f35650c5a339fc286167b4905ff2e
tlagoda <thibautlagoda@hotmail.fr>
2023-07-20 18:40:59 +0200

    log cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use((err, req, res, next) => {
+    const logger = new Logger('App');
+
+    if (err.name === 'UnauthorizedError' && err.code === 'ERR_ASSERTION') {
+      logger.error('CORS error:', err.message);
+    }
+
+    next(err);
+  });
 
   app.enableCors({
     origin: ['http://tldl.fr', 'https://tldl.fr', 'http://localhost:3000'],
