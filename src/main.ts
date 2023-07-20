a4d66a472507c0ec0df1aa82d7bff9b40d23b12f
tlagoda <thibautlagoda@hotmail.fr>
2023-07-20 19:58:00 +0200

    redirect middleware

--- src/main.ts
+++ src/main.ts
@@ -1,28 +1,14 @@
+import { HttpsRedirectMiddleware } from './middleware/https-redirect.middleware';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.use((err, req, res, next) => {
-    const logger = new Logger('App');
-
-    if (err) {
-      logger.error('CORS error:', err.message);
-    }
-
-    next(err);
-  });
+  app.use(new HttpsRedirectMiddleware().use);
 
   app.enableCors({
-    origin: [
-      'http://tldl.fr',
-      'https://tldl.fr',
-      'http://localhost:3000',
-      'https://linkedinai.herokuapp.com/',
-      'http://linkedinai.herokuapp.com/',
-    ],
+    origin: ['http://tldl.fr', 'https://tldl.fr', 'http://localhost:3000'],
     allowedHeaders: 'Content-Type,Authorization',
     methods: 'GET,POST',
   });
